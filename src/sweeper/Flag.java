e497511b78f0d8e9a7778857aff215db5b698775
oIITiMicT <backawork@gmail.com>
2021-04-25 23:19:22 +0200

    init project

---
+++ src/sweeper/Flag.java
@@ -0,0 +1,53 @@
+package sweeper;
+
+class Flag {
+    private Matrix flagMap;
+    private int countOfClosedBoxes;
+
+    void start() {
+        flagMap = new Matrix(Box.CLOSED);
+        countOfClosedBoxes = Ranges.getSize().x * Ranges.getSize().y;
+    }
+
+    Box get(Coord coord) {
+        return flagMap.get(coord);
+    }
+
+    void toggleFlagedToBox(Coord coord) {
+        switch (flagMap.get(coord)) {
+            case FLAGED: setClosedToBox(coord); break;
+            case CLOSED: setFlagedToBox(coord); break;
+        }
+    }
+
+    private void setClosedToBox(Coord coord) {
+        flagMap.set(coord, Box.CLOSED);
+    }
+
+    void setOpenedToBox(Coord coord) {
+        flagMap.set(coord, Box.OPENED);
+        countOfClosedBoxes--;
+    }
+
+    private void setFlagedToBox(Coord coord) {
+        flagMap.set(coord, Box.FLAGED);
+    }
+
+    int getCountOfCLosedBoxes() {
+        return countOfClosedBoxes;
+    }
+
+    void setBombedToBox(Coord coord) {
+        flagMap.set(coord, Box.BOMBED);
+    }
+
+    void setOpenedToClosedBombBox(Coord coord) {
+        if (flagMap.get(coord) == Box.CLOSED)
+            flagMap.set(coord, Box.OPENED);
+    }
+
+    void setNobombToFlagedSafeBox(Coord coord) {
+        if (flagMap.get(coord) == Box.FLAGED)
+            flagMap.set(coord, Box.NOBOMB);
+    }
+}
